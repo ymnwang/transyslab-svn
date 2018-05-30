dbda99d393a3e77be3662dedbf60d944d6130654
wangyim <wangyim6@mail2.susy.edu.cn>
2018-05-30 10:46:00 +0800

    固定实验设置开关，设为final

--- TranSysLab/src/main/java/com/transyslab/simcore/mlp/ExpSwitch.java
+++ TranSysLab/src/main/java/com/transyslab/simcore/mlp/ExpSwitch.java
@@ -4,19 +4,19 @@ package com.transyslab.simcore.mlp;
  * Created by WangYimin on 2018/3/2.
  */
 public class ExpSwitch {
-	public static boolean DOUBLE_LOOP = false;
-	public static boolean CAP_CTRL = false;
-	public static boolean MAX_ACC_CTRL = false;
-	public static boolean ACC_SMOOTH = false;//目前有BUG
-	public static boolean APPROACH_CTRL = false;
-	public static boolean CF_CURVE = false;
-	public static boolean SPD_BUFFER = false;
-	public static boolean VIRTUAL_RELEASE = false;
+	public final static boolean DOUBLE_LOOP = false;
+	public final static boolean CAP_CTRL = false;
+	public final static boolean MAX_ACC_CTRL = true;
+	public final static boolean ACC_SMOOTH = false;//目前有BUG
+	public final static boolean APPROACH_CTRL = true;
+	public final static boolean CF_CURVE = false;
+	public final static boolean SPD_BUFFER = false;
+	public final static boolean VIRTUAL_RELEASE = false;
 
-	public static double MAX_ACC = 4.0;
-	public static double MAX_DEC = -7.0;
-	public static double APPROACH_SPD = 60.0/3.6;
-	public static int SPD_BUFFER_VAL = 5;
-	public static double CF_VT_END = 200.0;
-	public static double CF_VT = 120.0/3.6;
+	public final static double MAX_ACC = 4.0;
+	public final static double MAX_DEC = -7.0;
+	public final static double APPROACH_SPD = 60.0/3.6;
+	public final static int SPD_BUFFER_VAL = 5;
+	public final static double CF_VT_END = 200.0;
+	public final static double CF_VT = 120.0/3.6;
 }
